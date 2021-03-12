2bdb54e26c8d66d4d328616932b2aea960186827
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-12 20:20:00 +0400

    add new project

---
+++ src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 24;
+        int c = a + b;
+        System.out.println("C value is: " + c);
+    }
+}
