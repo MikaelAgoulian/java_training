6f892f6d04790b6a684dcc6759fc2e8173a7fa50
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-15 20:48:43 +0400

    add new file Lesson2

---
+++ src/Lesson2.java
@@ -0,0 +1,37 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        int age[];
+        age = new int[5];
+        age[0] = 30;
+
+
+        age[1] = 33;
+        age[2] = 36;
+        age[3] = 44;
+        age[4] = 27;
+
+        String name[] = {"A", "B", "C", "D", "E"};
+
+        for (int i = 0; i < 5; i++) {
+            if (age[i]%2==0)
+                System.out.println(name[i]);
+        }
+
+//        int x = 0;
+//        while (x < 5) {
+//            if (age[x]%2==0)
+//                System.out.println(name[x]);
+//            x++;
+//        }
+
+
+//        System.out.println(age[0]%2==0?name[0]:"");
+//        System.out.println(age[1]%2==0?name[1]:"");
+//        System.out.println(age[2]%2==0?name[2]:"");
+//        System.out.println(age[3]%2==0?name[3]:"");
+//        System.out.println(age[4]%2==0?name[4]:"");
+
+    }
+}
+
