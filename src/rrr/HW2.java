459b03bdfc9b5f4c2ee4008b1080ea8ade1438e3
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-19 20:45:16 +0400

    homeWork

---
+++ src/rrr/HW2.java
@@ -0,0 +1,11 @@
+package rrr;
+
+public class HW2 {
+    public static void main(String[] args) {
+        int arr[];
+        arr = new int[5];
+        int i;
+        for (i = 0; i < 5; i++);
+//            arr = add(5, arr, i); ;
+    }
+}
