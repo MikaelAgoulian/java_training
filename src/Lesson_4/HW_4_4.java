5690711a4bb2b9ef525c29ab744ab69ea10f4041
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-21 23:59:50 +0400

    HW_4_4

--- src/Lesson_4/HW_4_4.java
+++ src/Lesson_4/HW_4_4.java
@@ -7,16 +7,23 @@ public class HW_4_4 {
         int dayNumber4[] = {1, 2, 3, 4, 5, 6, 7};
         String name4[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-        Scanner input = new Scanner(System.in);
-            System.out.print("Please enter any number of day: ");
+        System.out.print("Please enter any number of day: ");
+            Scanner input = new Scanner(System.in);
                 int number = input.nextInt();
 
-        for (int i = 0; i < dayNumber4.length; i++) {
-            if (number == dayNumber4[i]) {
-                System.out.println(name4[i]);
-            }
+                int i =0;
+        do {
+            System.out.println(name4[i]);
+            i++;
+        }
+        while (i < number);
+//
+//        for (int i = 0; i < dayNumber4.length; i++) {
+//            if (number == dayNumber4[i]) {
+//                System.out.println(name4[i]);
+//            }
 //            if (i == number) {
 //                break;
-            }
+//            }
         }
     }
