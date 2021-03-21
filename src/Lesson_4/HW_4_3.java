4b5be71baefb98655e127aa3ee640e773303d331
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-21 19:55:55 +0400

    HW_4_3_edit

--- src/Lesson_4/HW_4_3.java
+++ src/Lesson_4/HW_4_3.java
@@ -2,18 +2,40 @@ package Lesson_4;
 import java.util.Scanner;
 
 public class HW_4_3 {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
-        int dayNumber [] = {1, 2, 3, 4, 5, 6, 7};
+        int dayNumber[] = {1, 2, 3, 4, 5, 6, 7};
         String name[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter any number of day: ");
-        int number = input.nextInt();
-            for (int i = 0; i < dayNumber.length; i++ ) {
-                if (number == dayNumber[i])
-                    System.out.println("The day " + dayNumber[i] + " is " + name[i]);
-            }
-        System.out.println("You entered wrong day number");
-    }
+            System.out.print("Please enter any number of day: ");
+                int number = input.nextInt();
+                int i = 0;
+
+                switch (number){
+                    case 1:
+                        System.out.println("The day is Monday");
+                        break;
+                    case 2:
+                        System.out.println("The day is Tuesday");
+                        break;
+                    case 3:
+                        System.out.println("The day is Wednesday");
+                        break;
+                    case 4:
+                        System.out.println("The day is Thursday");
+                        break;
+                    case 5:
+                        System.out.println("The day is Friday");
+                        break;
+                    case 6:
+                        System.out.println("The day is Saturday");
+                        break;
+                    case 7:
+                        System.out.println("The day is Sunday");
+                        break;
+                    default:
+                        System.out.println("Not valid day");
+                }
+        }
 }
--- src/Lesson_4/HW_4_4.java
+++ src/Lesson_4/HW_4_4.java
@@ -2,13 +2,21 @@ package Lesson_4;
 import java.util.Scanner;
 
 public class HW_4_4 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        int dayNumber4 [] = {1, 2, 3, 4, 5, 6, 7};
-        String name4 [] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int dayNumber4[] = {1, 2, 3, 4, 5, 6, 7};
+        String name4[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter any number of day: ");
-        int number = input.nextInt();
+            System.out.print("Please enter any number of day: ");
+                int number = input.nextInt();
 
+        for (int i = 0; i < dayNumber4.length; i++) {
+            if (number == dayNumber4[i]) {
+                System.out.println(name4[i]);
+            }
+//            if (i == number) {
+//                break;
+            }
+        }
     }
-}
