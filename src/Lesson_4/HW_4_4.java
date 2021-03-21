360daa9765cf758fd32d5a1be7c398068dc1c36b
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-21 18:15:21 +0400

    HW_4_3_edit

---
+++ src/Lesson_4/HW_4_4.java
@@ -0,0 +1,14 @@
+package Lesson_4;
+import java.util.Scanner;
+
+public class HW_4_4 {
+    public static void main (String[] args){
+
+        int dayNumber4 [] = {1, 2, 3, 4, 5, 6, 7};
+        String name4 [] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter any number of day: ");
+        int number = input.nextInt();
+
+    }
+}
