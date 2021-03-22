88c2e16596ab321ef096a05d082791ca3c6ff74b
MikaelAgoulian <mikael.agoulian@picsart.com>
2021-03-22 12:41:24 +0400

    HW_4_3-EDIT

--- src/Lesson_4/HW_4_3.java
+++ src/Lesson_4/HW_4_3.java
@@ -1,41 +1,47 @@
 package Lesson_4;
+
 import java.util.Scanner;
 
 public class HW_4_3 {
     public static void main(String[] args) {
 
         int dayNumber[] = {1, 2, 3, 4, 5, 6, 7};
+
         String name[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-
+//                          0           1           2           3           4       5           6
         Scanner input = new Scanner(System.in);
-            System.out.print("Please enter any number of day: ");
-                int number = input.nextInt();
-                int i = 0;
+        System.out.print("Please enter any number of day: ");
+        int number = input.nextInt();
 
-                switch (number){
-                    case 1:
-                        System.out.println("The day is Monday");
-                        break;
-                    case 2:
-                        System.out.println("The day is Tuesday");
-                        break;
-                    case 3:
-                        System.out.println("The day is Wednesday");
-                        break;
-                    case 4:
-                        System.out.println("The day is Thursday");
-                        break;
-                    case 5:
-                        System.out.println("The day is Friday");
-                        break;
-                    case 6:
-                        System.out.println("The day is Saturday");
-                        break;
-                    case 7:
-                        System.out.println("The day is Sunday");
-                        break;
-                    default:
-                        System.out.println("Not valid day");
-                }
-        }
+        if (number >= 1 && number <= 7)
+            System.out.println("The day for: " + number + " is: " + name[number - 1]);
+        else
+            System.out.println("Not valid day");
+
+//        switch (number) {
+//            case 1:
+//                System.out.println("The day is Monday");
+//                break;
+//            case 2:
+//                System.out.println("The day is Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("The day is Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("The day is Thursday");
+//                break;
+//            case 5:
+//                System.out.println("The day is Friday");
+//                break;
+//            case 6:
+//                System.out.println("The day is Saturday");
+//                break;
+//            case 7:
+//                System.out.println("The day is Sunday");
+//                break;
+//            default:
+//                System.out.println("Not valid day");
+//        }
+    }
 }
---
+++ src/Lesson_4/HW_4_5.java
@@ -0,0 +1,7 @@
+package Lesson_4;
+
+public class HW_4_5 {
+    public static void main (String[] args){
+
+    }
+}
